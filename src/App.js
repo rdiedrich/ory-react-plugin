eaccec58beb16b23101ec097dcf979a4f6c9c4de
Rüdiger Diedrich <email@rdiedrich.org>
2019-03-06 12:31:02 +0100

    setup ory editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+// The editor core
+import Editor, { Editable, createEmptyState } from 'ory-editor-core'
+import 'ory-editor-core/lib/index.css' // we also want to load the stylesheets
+
+// Require our ui components (optional). You can implement and use your own ui too!
+import { Trash, DisplayModeToggle, Toolbar } from 'ory-editor-ui'
+import 'ory-editor-ui/lib/index.css'
+
+// Load some exemplary plugins:
+import slate from 'ory-editor-plugins-slate' // The rich text area plugin
+import 'ory-editor-plugins-slate/lib/index.css' // Stylesheets for the rich text area plugin
+import parallax from 'ory-editor-plugins-parallax-background' // A plugin for parallax background images
+import 'ory-editor-plugins-parallax-background/lib/index.css' // Stylesheets for parallax background images
+// require('react-tap-event-plugin')() // react-tap-event-plugin is required by material-ui which is used by ory-editor-ui so we need to call it here
+
+// Define which plugins we want to use. We only have slate and parallax available, so load those.
+const plugins = {
+  content: [slate()], // Define plugins for content cells. To import multiple plugins, use [slate(), image, spacer, divider]
+  layout: [parallax({ defaultPlugin: slate() })] // Define plugins for layout cells
+}
+
+
+// Creates an empty editable
+const content = createEmptyState()
+
+
+// Instantiate the editor
+const editor = new Editor({
+  plugins,
+  // pass the content state - you can add multiple editables here
+  editables: [content],
+})
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo"/>
+      <h2>Welcome to React</h2>
+      </div>
+
+      {/* Content area */}
+      <Editable editor={editor} id={content.id}/>
+
+      {/*  Default user interface  */}
+      <Trash editor={editor}/>
+      <DisplayModeToggle editor={editor}/>
+      <Toolbar editor={editor}/>
       </div>
     );
   }
